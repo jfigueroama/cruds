f3a4033f97a4e2aad77f96a7ab38ca0010d2d0ab
Jose Figueroa Martinez <coloso@gmail.com>
2016-11-23 16:14:27 -0600

    Agregando de nuevo bootstrap para que se vea decente.

--- turioax/src/views/layout.html.php
+++ turioax/src/views/layout.html.php
@@ -4,6 +4,35 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <title>Captura del Sistema de Turismo.</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="<?php echo site_url(); ?>public/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo site_url(); ?>public/css/prettify.css" />
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="<?php echo site_url(); ?>public/css/styles.css" rel="stylesheet">
+    <link href="<?php echo site_url(); ?>public/css/bootstrap-modal.css" rel="stylesheet">
+    <link href="<?php echo site_url(); ?>public/css/bootstrap-modal-bs3patch.css" rel="stylesheet">
+
+<!--   <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script> -->
+    <!-- <script src="<?php echo site_url(); ?>public/js/jquery-2.1.1.js"></script> -->
+    <script src="<?php echo site_url(); ?>public/js/jquery-1.9.1.js"></script>
+    <script src="<?php echo site_url(); ?>public/js/prettify.js"></script>
+    <script src="<?php echo site_url(); ?>public/js/jquery.bsAlerts.js"></script>
+    <script src="<?php echo site_url(); ?>public/js/scripts.js"></script>
+   <!--  <script src="<?php echo site_url(); ?>public/js/jquery.min.js"></script> -->
+    
+
+    <script src="<?php echo site_url(); ?>public/js/bootstrap.js"></script>
+
+    <script src="<?php echo site_url(); ?>public/js/bootstrap.min.js"></script>
+    <script src="<?php echo site_url(); ?>public/js/bootstrap-modalmanager.js"></script>
+    <script src="<?php echo site_url(); ?>public/js/bootstrap-modal.js"></script>
+    <script src="<?php echo site_url(); ?>public/js/interact.js"></script>
+    <script src="<?php echo site_url(); ?>public/js/modernizr.js"></script>
+
+
   </head>
   <body><?php echo content(); ?></body>
 </html>
